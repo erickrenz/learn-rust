720de22df7ad87696ebdb18274b306ed409026c3
krenzew <54631709+krenzew@users.noreply.github.com>
2022-12-07 10:16:50 +0100

    update implementation

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,22 +4,33 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
         width: dbg!(30 * scale),
         height: 50,
     };
+    let square1 = Rectangle::square(3);
+    dbg!(&square1);
 
     // dbg! both takes and returns ownership of the value it's given.
     dbg!(&rect1);
 
     println!(
         "The area of this rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
